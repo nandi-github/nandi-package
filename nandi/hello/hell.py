c71f3adcc578c05ea7367654f7202ae59bd30871
nandi-github <satish@nandi.net>
2019-06-24 20:55:47 -0700

    first commit

---
+++ nandi/hello/hell.py
@@ -0,0 +1,21 @@
+# Module to import data from STOCK market
+# Refer to this webpage for more details
+#       http://theautomatic.net/yahoo_fin-documentation/#get_data
+#
+#
+#### Credit to :
+#         Yahoo_fin is a Python 3 package I wrote to scrape historical stock price data, as well as
+#           to provide current information on market caps, dividend yields, and which stocks comprise
+#           the major exchanges. Additional functionality includes scraping income statements, balance
+#           sheets, cash flows, holder information, and analyst data. The package includes the ability to
+#           get live stock prices, capture cryptocurrency data, and get the most actively traded stocks
+#           on a current trading day
+#
+# sudo /Applications/Python\ 3.6/Install\ Certificates.command
+# Packeges required are:
+#       yahoo_fin : pip install yahoo_fin
+#                       pip install yahoo_fin --upgrade if you have installed a older version
+#
+
+def hell_x():
+    print ("Welcome to HELL")
